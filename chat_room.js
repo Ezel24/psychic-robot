96a7524ce9054990ad9de9317f42a4cca2dcd7b7
Ezel24 <134762823+Ezel24@users.noreply.github.com>
2023-10-08 11:45:08 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBJlgkA64Pd35T-jECG4J0z163ukYP6SCQ",
+    authDomain: "qalluliput.firebaseapp.com",
+    databaseURL: "https://qalluliput-default-rtdb.firebaseio.com",
+    projectId: "qalluliput",
+    storageBucket: "qalluliput.appspot.com",
+    messagingSenderId: "1059793449893",
+    appId: "1:1059793449893:web:1e64fc0969cef37e26ee4c"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
